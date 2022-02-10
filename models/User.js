7f14f0e63016d840a7131f47de860d591ca9edbe
Artur Mucha <arti.mucha@gmail.com>
2022-02-10 20:16:58 +0100

    create User model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+import Password from 'services/password';
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+  },
+  posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }]
+},
+{
+  toJSON :{
+    transform(doc, ret) {
+      delete ret.password;
+      delete ret.__v;
+    }
+  }
+});
+
+UserSchema.pre('save', async function(done) {
+  if(this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
+const User = mongoose.models.User || mongoose.model('User', UserSchema);
+
+export default User;

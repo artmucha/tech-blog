29cb0b2ec7201e27c2c48c22063238503be12ea9
Artur Mucha <arti.mucha@gmail.com>
2022-02-16 19:33:30 +0100

    add createFile func to utils

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,27 @@
+const fs = require('fs');
+
 export const formDate = date => {
   const newDate = new Date(date);
   return Intl.DateTimeFormat('pl-PL', {dateStyle: 'long'}).format(new Date(date));
 };
+
+export const makeSlug = text => {
+  let slug = text.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '-');
+  return slug;
+};
+
+export const createFile = (image, name) => {
+  const data = image.replace(/^data:image\/\w+;base64,/, "");
+  const mime = image.split('/')[1].split(';')[0];
+  const buffer = Buffer.from(data, 'base64');
+
+  fs.writeFile(`/public/posts/${name}.${mime}`, buffer, 'base64', (err) => {
+    if (err) return console.log(err);
+  });
+
+  const url = `/${name}.${mime}`;
+
+  console.log(url)
+
+  return url;
+}
